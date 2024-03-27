d57d45d26df75e43da90b50797e4f8c9a0e37fa8
TomxBC <tobecerra@itba.edu.ar>
2024-03-27 11:34:00 -0300

    Implementation of application and child procesess pipes.

--- application/app.c
+++ application/app.c
@@ -2,4 +2,83 @@
 
 int main(int argc, char * argv[]) {
     
+    int pid, status;
+
+    int file_index = 0; 
+
+    unsigned int amount_of_children = get_children_amount(argc-1);
+
+    pid_t pid_children[amount_of_children];
+
+    int fd_in[PIPE_FILEDESCRIPTORS];
+    int fd_out[PIPE_FILEDESCRIPTORS];
+
+    if (pipe(fd_in) == -1) {
+        perror("ERROR: error when creating pipe");
+        exit(EXIT_FAILURE);
+    }
+    if (pipe(fd_out) == -1) {
+        perror("ERROR: error when creating pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    for(int i=0; i < amount_of_children; i++) {
+        pid = fork();
+        if(pid < 0){
+            perror("ERROR: error when forking");
+            exit(EXIT_FAILURE);
+        } else {
+            if(pid == 0){
+                char * child_argv[] = {"child", NULL};
+                char * child_env[] = {NULL};
+
+                close(fd_in[PIPE_WRITE_END]);
+                close(fd_out[PIPE_READ_END]);
+
+                close(STDIN);
+                dup(fd_in[PIPE_READ_END]);
+                close(fd_in[PIPE_READ_END]);
+
+                close(STDOUT);
+                dup(fd_out[PIPE_WRITE_END]);
+                close(fd_out[PIPE_WRITE_END]);
+            
+                if(execve("child", child_argv, child_env) == -1){
+                    perror("ERROR: error when executing child");
+                    exit(EXIT_FAILURE);
+                }
+            }
+            close(fd_in[PIPE_READ_END]);
+            close(fd_out[PIPE_WRITE_END]);
+
+            close(STDOUT);
+            dup(fd_in[PIPE_READ_END]);
+            close(fd_in[PIPE_READ_END]);
+
+            close(STDIN);
+            dup(fd_out[PIPE_WRITE_END]);
+            close(fd_out[PIPE_WRITE_END]);
+        }
+    }
+
+     for(int i = 0; i < amount_of_children; i++){
+        if(waitpid(pid_children[i], &status, 0) == -1){    //WUNTRACED:if any of the child processes have stopped waitpid() will return immediately
+            perror("ERROR: error when waiting for child process");
+            exit(EXIT_FAILURE);
+        }else{
+            printf("The child %d has died\n", pid_children[i]); //Despues borrar
+        }
+    }
+
+}
+
+int get_children_amount(int amount_of_files) {
+
+    if(amount_of_files > INFLEX_POINT) {
+        return (int)(amount_of_files)*0.05;
+    }
+    if( amount_of_files < MIN_CHILDREN ) {
+        return amount_of_files;
+    }
+    return MIN_CHILDREN;
 }
--- application/app.h
+++ application/app.h
@@ -5,6 +5,15 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdlib.h>
+
+#define MIN_CHILDREN 5
+#define INFLEX_POINT 50
+#define PIPE_FILEDESCRIPTORS 2
+#define PIPE_READ_END 0
+#define PIPE_WRITE_END 1
+#define STDOUT 1
+#define STDIN 0
 
 #endif
 
